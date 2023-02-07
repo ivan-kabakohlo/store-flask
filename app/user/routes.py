ed598c7b5f0c64636c2d104cfa6a736b5af48907
ivan_kabakohlo <ivan.kabakohlo@gmail.com>
2023-02-07 23:35:07 +0200

    Add User Routes

--- app/user/routes.py
+++ app/user/routes.py
@@ -1,6 +1,25 @@
+from flask import jsonify
+
+from app.extensions import db
+from app.models.user import User
 from app.user import bp
+from app.user.serializers import UserSerializer
+
+user_list_serializer = UserSerializer(many=True)
+user_serializer = UserSerializer()
 
 
 @bp.route('/users', methods=['GET'])
-def users():
-    return 'Users'
+def read_user_list():
+    users = db.session.query(User).all()
+    return user_list_serializer.dump(users)
+
+
+@bp.route('/users/<int:id>', methods=['GET'])
+def read_user(id):
+    user = db.session.query(User).get(id)
+
+    if user is None:
+        return jsonify({'message': 'User not found.'}), 404
+
+    return user_serializer.dump(user)
